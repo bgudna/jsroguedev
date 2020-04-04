9319ba01013a72a4532ac22c0793147d406724b4
bgudna <b@bgudna.com>
2020-04-04 12:28:51 +0200

    cleanup, player block and basic movement added

---
+++ main.js
@@ -0,0 +1,19 @@
+canvas = document.querySelector('canvas');
+ctx = canvas.getContext('2d');
+ctx.fillRect(0,0,20,20,20);
+
+x = y = 0;
+
+document.querySelector('html').onkeypress = function(e) {
+    if(e.key == 'w' || e.key == 'ArrowUp') y--;
+    if(e.key == 's' || e.key == 'ArrowDown') y++;
+    if(e.key == 'a' || e.key == 'ArrowLeft') x--;
+    if(e.key == 'd' || e.key == 'ArrowRight') x++;
+};
+
+function draw() {
+    ctx.clearRect(0,0,1000,1000);
+    ctx.fillRect (x*20,y*20, 20, 20);
+}
+
+setInterval(draw, 15);
